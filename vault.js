2af40d9b31b915663b06debc07adb169eb080761
Luke-mc <luke_mcglynn@yahoo.com>
2017-05-18 20:34:51 -1000

    done

--- vault.js
+++ vault.js
@@ -1,4 +1,38 @@
 'use strict';
 module.exports = function() {
-  
+
+   var vault = {};
+
+   function setValue(key,value){
+
+    vault.key = key;
+
+    vault.value = value;
+
+   };
+
+   function getValue(key){
+
+   if(vault.key){
+
+    return vault.value
+
+    }
+    else{
+
+      return null;
+    }
+
+   };
+
+  return{
+
+   setValue:setValue,
+   getValue:getValue,
+
+  };
+
+
 };
+
+
